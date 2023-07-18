216432b31798ba0af97f92412b4da95dfa31c2e6
g4br100 <124401134+g4br100@users.noreply.github.com>
2023-07-18 16:30:19 +0200

    aggiunto migration

---
+++ database/migrations/2023_07_18_140257_create_comics_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+
+            $table->tinytext("title");
+            $table->text("description");
+            $table->string("thumb");
+            $table->smallint("price");
+            $table->string("series", 50);
+            $table->date("sale_date");
+            $table->string("type", 50);
+            $table->text("artists");
+            $table->text("writers");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,4 @@
+<header class="p-3">
+    <img class="logo" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Logo">
+    <span>DC COMICS</span>
+</header>
