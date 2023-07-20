c152b84480fdec98ae18bf7af4fd7e714f520614
g4br100 <124401134+g4br100@users.noreply.github.com>
2023-07-20 21:37:14 +0200

    aggiornamento validazione

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -3,33 +3,42 @@
 @section('content')
 
     <div class="container my-3">
-        <h1>Card: {{$comic->title}}</h1>
-        <div class="row g-4">
-            <div class="col">
-                <div class="card comic text-center">
-                    <a href="{{ route("comics.show", $comic->id)}}"><div>{{$comic["title"]}}</div></a>
-                    <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}" class="thumb"> 
-                </div>
-                <div class="mt-3">
-                    <div>Descrizione: {{$comic["description"]}}</div>
-                    <div>Prezzo: {{$comic["price"]}}</div>
-                    <div>Serie: {{$comic["series"]}}</div>
-                    <div>Data di uscita: {{$comic["sale_date"]}}</div>
-                    <div>Categoria: {{$comic["type"]}}</div>
-                    <div>Artisti: {{$comic["artists"]}}</div>
-                    <div>Scrittori: {{$comic["writers"]}}</div>
+        <h1>{{$comic->title}}</h1>
+        <div class="row mt-3">
+            <div class="col d-flex">
+                
+                <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}" class="thumb"> 
+                
+                <div class="ms-3">
+                    <div class="mb-2"><span class="badge text-bg-primary">Descrizione:</span> {{$comic["description"]}}</div>
+                    <div class="mb-2"><span class="badge text-bg-primary">Prezzo:</span> {{$comic["price"]}}</div>
+                    <div class="mb-2"><span class="badge text-bg-primary">Serie:</span> {{$comic["series"]}}</div>
+                    <div class="mb-2"><span class="badge text-bg-primary">Data di uscita:</span> {{$comic["sale_date"]}}</div>
+                    <div class="mb-2"><span class="badge text-bg-primary">Categoria:</span> {{$comic["type"]}}</div>
+                    <div class="mb-2"><span class="badge text-bg-primary">Artisti:</span> {{$comic["artists"]}}</div>
+                    <div class="mb-2"><span class="badge text-bg-primary">Scrittori:</span> {{$comic["writers"]}}</div>
                 </div>
             </div>
-            <a href="{{route("home")}}">Torna alla lista dei fumetti</a>
-            <a href="{{route("comics.edit", $comic->id)}}">Modifica fumetto</a>
-            <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
-                @csrf
-                @method('DELETE')
-                <input type="submit" class="btn btn-danger" value="Elimina fumetto">
+        </div>
+        <div class="row">
+            <div class="col d-flex mt-3">
+            
+                <div>
+                    <a class="btn btn-success me-3" href="{{route("comics.edit", $comic->id)}}">Modifica fumetto</a>
+                </div>
+    
+                <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" class="btn btn-danger me-3" value="Elimina fumetto">
+                </form>
 
-            </form>
+                <div>
+                    <a class="btn btn-secondary" href="{{route("home")}}">Torna alla lista dei fumetti</a>
+                </div>
+                
+            </div>
         </div>
     </div>
-
 </div>
 @endsection
